54e9eef45fde76e3b43f44b862aead29c7aff82c
piupuer <piupuer@gmail.com>
2022-09-21 17:05:18 +0800

    add trace id middleware

---
+++ middleware/go.mod
@@ -0,0 +1,15 @@
+module github.com/go-cinch/common/middleware
+
+go 1.18
+
+require (
+	github.com/go-kratos/kratos/v2 v2.5.0
+	go.opentelemetry.io/otel/trace v1.7.0
+)
+
+require (
+	github.com/go-playground/form/v4 v4.2.0 // indirect
+	go.opentelemetry.io/otel v1.7.0 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0 // indirect
+)
---
+++ middleware/traceid.go
@@ -0,0 +1,21 @@
+package middleware
+
+import (
+	"context"
+	"github.com/go-kratos/kratos/v2/middleware"
+	"github.com/go-kratos/kratos/v2/transport"
+	"go.opentelemetry.io/otel/trace"
+)
+
+func TraceId() middleware.Middleware {
+	return func(handler middleware.Handler) middleware.Handler {
+		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
+			if span := trace.SpanContextFromContext(ctx); span.HasTraceID() {
+				if tr, ok := transport.FromServerContext(ctx); ok {
+					tr.ReplyHeader().Set("trace-id", span.TraceID().String())
+				}
+			}
+			return handler(ctx, req)
+		}
+	}
+}
