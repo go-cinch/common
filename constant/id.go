c9a16406c65f5580f2b2df3d4f74a2d94a92b6f5
piupuer <piupuer@gmail.com>
2022-09-28 14:19:21 +0800

    add constant id

---
+++ constant/id.go
@@ -0,0 +1,49 @@
+package constant
+
+const (
+	UI0 uint64 = 0
+	UI1
+	UI2
+	UI3
+	UI4
+	UI5
+	UI6
+	UI7
+	UI8
+	UI9
+	UI10
+	UI11
+	UI12
+	UI13
+	UI14
+	UI15
+	UI16
+	UI17
+	UI18
+	UI19
+	UI20
+)
+
+const (
+	I0 int64 = 0
+	I1
+	I2
+	I3
+	I4
+	I5
+	I6
+	I7
+	I8
+	I9
+	I10
+	I11
+	I12
+	I13
+	I14
+	I15
+	I16
+	I17
+	I18
+	I19
+	I20
+)
