b53511ab155f7f352de4fcbeea896d6d19ab11ba
piupuer <piupuer@gmail.com>
2022-10-15 15:59:11 +0800

    use snake case

--- utils/diff.go
+++ utils/diff.go
@@ -11,11 +11,19 @@ func CompareDiff(o interface{}, n interface{}, change interface{}) {
 	m2 := make(map[string]interface{}, 0)
 	Struct2StructByJson(&m1, o)
 	Struct2StructByJson(&m2, n)
+	m3 := make(map[string]interface{}, len(m1))
+	m4 := make(map[string]interface{}, len(m2))
+	for k, v := range m1 {
+		m3[SnakeCase(k)] = v
+	}
+	for k, v := range m2 {
+		m4[SnakeCase(k)] = v
+	}
 	defer func() {
 		err := recover()
 		if err != nil {
 			log.Warn("CompareDiff, pls check params: %v", err)
 		}
 	}()
-	diff.Merge(m1, m2, change)
+	diff.Merge(m3, m4, change)
 }
