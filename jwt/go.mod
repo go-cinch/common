8f8726f7f34791177bdb899e513b5c556006b245
piupuer <piupuer@gmail.com>
2022-10-25 12:21:39 +0800

    add metadata

--- jwt/go.mod
+++ jwt/go.mod
@@ -4,8 +4,14 @@ go 1.18
 
 require (
 	github.com/go-cinch/common/copierx v0.0.0-20221024063233-aec4d9648be5
+	github.com/go-kratos/kratos/v2 v2.5.2
 	github.com/golang-jwt/jwt/v4 v4.4.2
 	github.com/golang-module/carbon/v2 v2.1.9
 )
 
-require github.com/jinzhu/copier v0.3.5 // indirect
+require (
+	github.com/go-playground/form/v4 v4.2.0 // indirect
+	github.com/jinzhu/copier v0.3.5 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0 // indirect
+)
--- jwt/jwt.go
+++ jwt/jwt.go
@@ -3,36 +3,43 @@ package jwt
 import (
 	"context"
 	"github.com/go-cinch/common/copierx"
+	"github.com/go-kratos/kratos/v2/metadata"
+	"github.com/go-kratos/kratos/v2/transport"
 	jwtV4 "github.com/golang-jwt/jwt/v4"
 	"github.com/golang-module/carbon/v2"
 	"strings"
 )
 
 const (
-	ClaimAuthorityId = "authorityId"
-	ClaimExpires     = "exp"
+	ClaimCode     = "code"
+	ClaimPlatform = "platform"
+	ClaimExpires  = "exp"
 )
 
 type User struct {
-	Code string
+	Token    string
+	Code     string
+	Platform string
 }
 
 type user struct{}
 
 func NewServerContext(ctx context.Context, claims jwtV4.Claims) context.Context {
 	if mClaims, ok := claims.(jwtV4.MapClaims); ok {
-		if v, ok2 := mClaims[ClaimAuthorityId].(string); ok2 {
-			return NewServerContextByCode(ctx, v)
+		u := new(User)
+		if v, ok2 := mClaims[ClaimCode].(string); ok2 {
+			u.Code = v
 		}
+		if v, ok3 := mClaims[ClaimPlatform].(string); ok3 {
+			u.Platform = v
+		}
+		ctx = NewServerContextByUser(ctx, *u)
 	}
 	return ctx
 }
 
-func NewServerContextByCode(ctx context.Context, code string) context.Context {
-	c := strings.TrimSpace(code)
-	if c != "" {
-		ctx = context.WithValue(ctx, user{}, &User{Code: c})
-	}
+func NewServerContextByUser(ctx context.Context, u User) context.Context {
+	ctx = context.WithValue(ctx, user{}, &u)
 	return ctx
 }
 
@@ -44,13 +51,50 @@ func FromServerContext(ctx context.Context) (u *User) {
 	return
 }
 
+func FromClientContext(ctx context.Context) (u *User) {
+	u = new(User)
+	if md, ok := metadata.FromServerContext(ctx); ok {
+		u.Code = md.Get("x-md-global-code")
+		u.Platform = md.Get("x-md-global-platform")
+	}
+	u.Token = TokenFromClientContext(ctx)
+	return
+}
+
+func TokenFromClientContext(ctx context.Context) (token string) {
+	if tr, ok := transport.FromServerContext(ctx); ok {
+		auths := strings.SplitN(tr.RequestHeader().Get("Authorization"), " ", 2)
+		if len(auths) == 2 && strings.EqualFold(auths[0], "Bearer") {
+			token = auths[1]
+			return
+		}
+	}
+	if md, ok := metadata.FromServerContext(ctx); ok {
+		token = md.Get("x-md-global-jwt")
+		return
+	}
+	return
+}
+
+func AppendToClientContext(ctx context.Context) context.Context {
+	u := FromClientContext(ctx)
+	ctx = metadata.AppendToClientContext(
+		ctx,
+		"x-md-global-code", u.Code,
+		"x-md-global-platform", u.Platform,
+		"x-md-global-jwt", u.Token,
+	)
+	return ctx
+}
+
 func (u *User) CreateToken(key, duration string) (token string, expires carbon.Carbon) {
 	expires = carbon.Now().AddDuration(duration)
 	claims := jwtV4.NewWithClaims(
 		jwtV4.SigningMethodHS512,
 		jwtV4.MapClaims{
-			ClaimAuthorityId: u.Code,
-			ClaimExpires:     expires.Timestamp(),
+			ClaimCode:     u.Code,
+			ClaimPlatform: u.Platform,
+			ClaimExpires:  expires.Timestamp(),
 		},
 	)
 	token, _ = claims.SignedString([]byte(key))
