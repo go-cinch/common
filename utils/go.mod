66f0700844f787c53bc787926a300a62ea5cc4fb
piupuer <piupuer@gmail.com>
2023-04-10 16:37:21 +0800

    redis client v9

--- utils/go.mod
+++ utils/go.mod
@@ -4,7 +4,6 @@ go 1.18
 
 require (
 	github.com/go-cinch/common/log v1.0.2
-	github.com/hibiken/asynq v0.24.0
 	github.com/pkg/errors v0.9.1
 	github.com/r3labs/diff/v3 v3.0.1
 	github.com/redis/go-redis/v9 v9.0.3
@@ -12,16 +11,12 @@ require (
 
 require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-kratos/kratos/v2 v2.6.1 // indirect
-	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/robfig/cron/v3 v3.0.1 // indirect
-	github.com/spf13/cast v1.5.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
-	golang.org/x/sys v0.7.0 // indirect
-	golang.org/x/time v0.3.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 )
--- utils/uri.go
+++ utils/uri.go
@@ -1,19 +1,57 @@
 package utils
 
 import (
-	"github.com/hibiken/asynq"
 	"github.com/pkg/errors"
 	"github.com/redis/go-redis/v9"
+	"net/url"
+	"strconv"
+	"strings"
 )
 
 func ParseRedisURI(uri string) (client redis.UniversalClient, err error) {
-	var opt asynq.RedisConnOpt
 	if uri != "" {
-		opt, err = asynq.ParseRedisURI(uri)
+		var u *url.URL
+		u, err = url.Parse(uri)
 		if err != nil {
+			err = errors.Errorf("invalid redis uri %s: %v", uri, err)
 			return
 		}
-		client = opt.MakeRedisClient().(redis.UniversalClient)
+		q := u.Query()
+
+		addrs := strings.Split(u.Host, ",")
+		var master, username, password, sentinelUsername, sentinelPassword string
+		var db int
+		master = q.Get("master")
+		username = u.User.Username()
+		if v, ok := u.User.Password(); ok {
+			password = v
+		}
+
+		if len(u.Path) > 0 {
+			xs := strings.Split(strings.Trim(u.Path, "/"), "/")
+			db, err = strconv.Atoi(xs[0])
+			if err != nil {
+				err = errors.Errorf("invalid db %s: %v", uri, err)
+				return
+			}
+		}
+
+		sentinel, _ := strconv.ParseBool(q.Get("sentinel"))
+		if sentinel {
+			sentinelUsername = username
+			sentinelPassword = password
+			username = ""
+			password = ""
+		}
+		client = redis.NewUniversalClient(&redis.UniversalOptions{
+			Addrs:            addrs,
+			MasterName:       master,
+			DB:               db,
+			Username:         username,
+			Password:         password,
+			SentinelUsername: sentinelUsername,
+			SentinelPassword: sentinelPassword,
+		})
 		return
 	}
 	err = errors.Errorf("invalid redis uri")
