9ac9a5f240dfd4981f4af7ebffd74a3ea11789e5
piupuer <piupuer@gmail.com>
2023-10-25 21:54:51 +0800

    [feat]add reason

--- constant/reason.go
+++ constant/reason.go
@@ -9,11 +9,13 @@ const (
 	IdempotentMissingToken    = "idempotent.token.missing"
 	IdempotentTokenExpired    = "idempotent.token.invalid"
 
-	TooManyRequests    = "too.many.requests"
-	DataNotChange      = "data.not.change"
-	DuplicateField     = "duplicate.field"
-	RecordNotFound     = "record.not.found"
-	NoPermission       = "no.permission"
+	TooManyRequests  = "too.many.requests"
+	DataNotChange    = "data.not.change"
+	DuplicateField   = "duplicate.field"
+	RecordNotFound   = "record.not.found"
+	NoPermission     = "no.permission"
+	InternalError    = "internal.error"
+	IllegalParameter = "illegal.parameter"
 
 	IncorrectPassword  = "login.incorrect.password"
 	SamePassword       = "login.same.password"
