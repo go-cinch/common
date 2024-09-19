b57bbb1d10cb02c7e6653ab5dd75667827717f91
piupuer <piupuer@gmail.com>
2024-09-19 10:47:48 +0800

    [fix]add quote

--- plugins/gorm/filter/filter.go
+++ plugins/gorm/filter/filter.go
@@ -3,17 +3,17 @@ package filter
 import "gorm.io/gen"
 
 type Filter interface {
-	// SELECT * FROM @@table WHERE id = @id LIMIT 1
+	// SELECT * FROM `@@table` WHERE `id` = @id LIMIT 1
 	GetByID(id uint64) gen.T
-	// SELECT * FROM @@table
+	// SELECT * FROM `@@table`
 	// {{where}}
 	//   {{if val != ""}}
 	//     {{if strings.HasPrefix(val, "%") && strings.HasSuffix(val, "%")}}
-	//       @@col LIKE concat('%', TRIM(BOTH '%' FROM @val), '%')
+	//       @@col LIKE CONCAT('%', TRIM(BOTH '%' FROM @val), '%')
 	//     {{else if strings.HasPrefix(val, "%")}}
-	//       @@col LIKE concat('%', TRIM(BOTH '%' FROM @val))
+	//       @@col LIKE CONCAT('%', TRIM(BOTH '%' FROM @val))
 	//     {{else if strings.HasSuffix(val, "%")}}
-	//       @@col LIKE concat(TRIM(BOTH '%' FROM @val), '%')
+	//       @@col LIKE CONCAT(TRIM(BOTH '%' FROM @val), '%')
 	//     {{else}}
 	//       @@col = @val
 	//     {{end}}
@@ -21,7 +21,7 @@ type Filter interface {
 	// {{end}}
 	// LIMIT 1
 	GetByCol(col, val string) gen.T
-	// SELECT * FROM @@table
+	// SELECT * FROM `@@table`
 	// {{if len(cols) == len(vals)}}
 	//  {{where}}
 	//     {{for i, col := range cols}}
@@ -29,11 +29,11 @@ type Filter interface {
 	//         {{if i == j}}
 	//           {{if val != ""}}
 	//             {{if strings.HasPrefix(val, "%") && strings.HasSuffix(val, "%")}}
-	//               @@col LIKE concat('%', TRIM(BOTH '%' FROM @val), '%') AND
+	//               @@col LIKE CONCAT('%', TRIM(BOTH '%' FROM @val), '%') AND
 	//             {{else if strings.HasPrefix(val, "%")}}
-	//               @@col LIKE concat('%', TRIM(BOTH '%' FROM @val)) AND
+	//               @@col LIKE CONCAT('%', TRIM(BOTH '%' FROM @val)) AND
 	//             {{else if strings.HasSuffix(val, "%")}}
-	//               @@col LIKE concat(TRIM(BOTH '%' FROM @val), '%') AND
+	//               @@col LIKE CONCAT(TRIM(BOTH '%' FROM @val), '%') AND
 	//             {{else}}
 	//               @@col = @val AND
 	//             {{end}}
@@ -45,22 +45,22 @@ type Filter interface {
 	// {{end}}
 	// LIMIT 1
 	GetByCols(cols, vals []string) gen.T
-	// SELECT * FROM @@table
+	// SELECT * FROM `@@table`
 	// {{where}}
 	//   {{if val != ""}}
 	//     {{if strings.HasPrefix(val, "%") && strings.HasSuffix(val, "%")}}
-	//       @@col LIKE concat('%', TRIM(BOTH '%' FROM @val), '%')
+	//       @@col LIKE CONCAT('%', TRIM(BOTH '%' FROM @val), '%')
 	//     {{else if strings.HasPrefix(val, "%")}}
-	//       @@col LIKE concat('%', TRIM(BOTH '%' FROM @val))
+	//       @@col LIKE CONCAT('%', TRIM(BOTH '%' FROM @val))
 	//     {{else if strings.HasSuffix(val, "%")}}
-	//       @@col LIKE concat(TRIM(BOTH '%' FROM @val), '%')
+	//       @@col LIKE CONCAT(TRIM(BOTH '%' FROM @val), '%')
 	//     {{else}}
 	//       @@col = @val
 	//     {{end}}
 	//   {{end}}
 	// {{end}}
 	FindByCol(col, val string) []gen.T
-	// SELECT * FROM @@table
+	// SELECT * FROM `@@table`
 	// {{if len(cols) == len(vals)}}
 	//  {{where}}
 	//     {{for i, col := range cols}}
@@ -68,11 +68,11 @@ type Filter interface {
 	//         {{if i == j}}
 	//           {{if val != ""}}
 	//             {{if strings.HasPrefix(val, "%") && strings.HasSuffix(val, "%")}}
-	//               @@col LIKE concat('%', TRIM(BOTH '%' FROM @val), '%') AND
+	//               @@col LIKE CONCAT('%', TRIM(BOTH '%' FROM @val), '%') AND
 	//             {{else if strings.HasPrefix(val, "%")}}
-	//               @@col LIKE concat('%', TRIM(BOTH '%' FROM @val)) AND
+	//               @@col LIKE CONCAT('%', TRIM(BOTH '%' FROM @val)) AND
 	//             {{else if strings.HasSuffix(val, "%")}}
-	//               @@col LIKE concat(TRIM(BOTH '%' FROM @val), '%') AND
+	//               @@col LIKE CONCAT(TRIM(BOTH '%' FROM @val), '%') AND
 	//             {{else}}
 	//               @@col = @val AND
 	//             {{end}}
