380a89ec65f8b3e0db76dd7e9fe9f9e03182d3b4
piupuer <piupuer@gmail.com>
2025-02-13 13:52:05 +0800

    [feat]idempotent use "not exist then set" replace "generate and check exist then delete"

--- idempotent/idempotent.go
+++ idempotent/idempotent.go
@@ -3,24 +3,20 @@ package idempotent
 import (
 	"context"
 	"strings"
-	"time"
 
 	"github.com/go-cinch/common/log"
-	"github.com/google/uuid"
 )
 
-// redis lua script(read => delete => get delete flag)
+// redis lua script(read => not exist set or exist return)
 const (
 	lua string = `
 local current = redis.call('GET', KEYS[1])
-if current == false then
-    return '-1';
+if current ~= false then
+    return '0';
 end
-local del = redis.call('DEL', KEYS[1])
-if del == 1 then
-     return '1';
-end
-return '0';
+redis.call('SET', KEYS[1], ARGV[1])
+redis.call('EXPIRE', KEYS[1], ARGV[2])
+return '1';
 `
 )
 
@@ -36,25 +32,18 @@ func New(options ...func(*Options)) *Idempotent {
 	return &Idempotent{ops: *ops}
 }
 
-func (i *Idempotent) Token(ctx context.Context) (token string) {
-	token = uuid.NewString()
-	if i.ops.redis != nil {
-		i.ops.redis.Set(ctx, strings.Join([]string{i.ops.prefix, token}, "_"), true, time.Duration(i.ops.expire)*time.Minute)
-	} else {
-		log.WithContext(ctx).Warn("please enable redis, otherwise the idempotent is invalid")
-	}
-	return
-}
-
 func (i *Idempotent) Check(ctx context.Context, token string) (pass bool) {
-	if i.ops.redis != nil {
-		res, err := i.ops.redis.Eval(ctx, lua, []string{strings.Join([]string{i.ops.prefix, token}, "_")}).Result()
-		if err != nil || res != "1" {
-			return
-		}
-	} else {
+	if i.ops.redis == nil {
 		log.WithContext(ctx).Warn("please enable redis, otherwise the idempotent is invalid")
+		return
 	}
-	pass = true
+	res, err := i.ops.redis.Eval(
+		ctx,
+		lua,
+		[]string{strings.Join([]string{i.ops.prefix, token}, "_")},
+		"1",
+		i.ops.expire,
+	).Result()
+	pass = err == nil && res == "1"
 	return
 }
